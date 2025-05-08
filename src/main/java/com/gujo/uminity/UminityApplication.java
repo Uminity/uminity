757772c20d5706503634027ff038979ac06c6c67
Iamcalmdown <ehsk2315@kyungsung.ac.kr>
2025-05-08 17:09:55 +0900

    Initial commit

---
+++ src/main/java/com/gujo/uminity/UminityApplication.java
@@ -0,0 +1,13 @@
+package com.gujo.uminity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UminityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UminityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gujo/uminity/UminityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gujo.uminity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UminityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

0df51626955661bc9f73a0456b5a0597069e7c75
Eddie Lee <devleehy@gmail.com>
2025-05-10 00:05:36 +0900

    feat: 회원가입 응답 DTO 구현

---
+++ src/main/java/com/gujo/uminity/auth/dto/RegisterResponseDto.java
@@ -0,0 +1,12 @@
+package com.gujo.uminity.auth.dto;
+
+import com.gujo.uminity.common.ResultStatus;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class RegisterResponseDto {
+
+    private ResultStatus result;
+}

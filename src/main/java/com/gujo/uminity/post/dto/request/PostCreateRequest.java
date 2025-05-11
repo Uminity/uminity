b5689c6796c38a4faa2fd47154fb35a9cd87b15b
Iamcalmdown <ehsk2315@kyungsung.ac.kr>
2025-05-11 17:49:15 +0900

    feat: Dto 역할과 관심사의 분리

---
+++ src/main/java/com/gujo/uminity/post/dto/request/PostCreateRequest.java
@@ -0,0 +1,29 @@
+package com.gujo.uminity.post.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class PostCreateRequest {
+    @NotBlank(message = "제목을 반드시 입력하세요")
+    private String title;
+
+    @NotBlank(message = "내용도 입력하세요")
+    private String content;
+}
+
+/*
+생각 정리
+
+Dto 에는 전달 객체여서 괜찮은데 Entity 에는 @Data 자제
+객체 스스로의 책임과 통제가 약화된다
+모든 필드에 퍼블릭한 접근 만들고 캡슐화와 무결성 해칠 수 있다.
+ */
+
+/*
+서버에서 생성되는 필드 postId랑 createdAt은 클라이언트가 보내지말고 서버에서 채워서 응답 DTO에 넣자
+책임과 관심사의 분리
+ */
+

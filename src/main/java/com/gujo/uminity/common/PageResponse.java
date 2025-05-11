f0659c524abcc30d0ca4d1bf25c9ed072cfba17d
Iamcalmdown <ehsk2315@kyungsung.ac.kr>
2025-05-11 17:50:47 +0900

    feat: common 패키지 응답객체 제네릭 설정 타입 지정 미지수

--- src/main/java/com/gujo/uminity/common/PageResponse.java
+++ src/main/java/com/gujo/uminity/common/PageResponse.java
@@ -11,8 +11,6 @@ import java.util.List;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-// 제네릭 타입을 쓴 이유가 페이징할 대상 DTO 타입을 나중에 지정하기 위해
-// 페이지만 할게 아니라 댓글이랑 좋아요 누른 사람들도 해보려고
 
 public class PageResponse<T> {
     private List<T> content;
@@ -21,6 +19,11 @@ public class PageResponse<T> {
 
     // 걍 DB 전체 게시글 수
     private long totalElements;
-    // 전체 게시글 수 / size 한거의 올림 버전
+    // 전체 구성되어야 되는 페이지 개수 == 전체 게시글 수 / size 한거의 올림 버전
     private long totalPages;
 }
+
+/*
+제네릭 타입을 쓴 이유가 페이징할 대상 DTO 타입을 나중에 지정하기 위해
+페이지만 할게 아니라 댓글이랑 좋아요 누른 사람들도 해보려고
+ */

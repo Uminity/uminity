4dd15f329aca47912ca560042f1403689e13ebb8
Eddie Lee <devleehy@gmail.com>
2025-05-12 09:24:54 +0900

    feat: AuthController 구현

--- src/main/java/com/gujo/uminity/auth/controller/AuthController.java
+++ src/main/java/com/gujo/uminity/auth/controller/AuthController.java
@@ -3,11 +3,10 @@ package com.gujo.uminity.auth.controller;
 import com.gujo.uminity.auth.dto.RegisterRequestDto;
 import com.gujo.uminity.auth.dto.RegisterResponseDto;
 import com.gujo.uminity.auth.service.AuthService;
-import com.gujo.uminity.common.ResultStatus;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,13 +19,10 @@ public class AuthController {
     private final AuthService authService;
 
     @PostMapping("/register")
-    public ResponseEntity<RegisterResponseDto> register(@RequestBody RegisterRequestDto registerRequestDto) {
-        RegisterResponseDto register = authService.register(registerRequestDto);
+    public ResponseEntity<RegisterResponseDto> register(@Validated @RequestBody RegisterRequestDto registerRequestDto) {
 
-        if (register.getResult().equals(ResultStatus.SUCCESS)) {
-            return ResponseEntity.ok(register);
-        } else {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(register);
-        }
+        RegisterResponseDto response = authService.register(registerRequestDto);
+
+        return ResponseEntity.ok(response);
     }
 }

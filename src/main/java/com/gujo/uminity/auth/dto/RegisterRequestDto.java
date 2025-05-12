1e4a50e6dd5474fe931fe9e4fb15f3cab3f03d9d
Eddie Lee <devleehy@gmail.com>
2025-05-12 09:16:19 +0900

    feat: RegisterRequestDto 구현

--- src/main/java/com/gujo/uminity/auth/dto/RegisterRequestDto.java
+++ src/main/java/com/gujo/uminity/auth/dto/RegisterRequestDto.java
@@ -1,8 +1,8 @@
 package com.gujo.uminity.auth.dto;
 
-import com.gujo.uminity.user.entity.Role;
-import java.util.ArrayList;
-import java.util.List;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -10,9 +10,17 @@ import lombok.Setter;
 @Setter
 public class RegisterRequestDto {
 
+    @NotBlank(message = "이름은 필수입니다.")
     private String name;
+
+    @NotBlank(message = "이메일은 필수입니다.")
+    @Email(message = "이메일 형식이 올바르지 않습니다.")
     private String email;
+
+    @NotBlank(message = "비밀번호는 필수입니다.")
     private String password;
+
+    @NotBlank(message = "휴대폰 번호는 필수입니다.")
+    @Pattern(regexp = "^01[0-9]-\\d{4}-\\d{4}$", message = "전화번호 형식이 올바르지 않습니다. (예: 010-1234-5678)")
     private String phone;
-    private List<Role> roles = new ArrayList<>();
 }

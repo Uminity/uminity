f66ef45fc42622f2f24e9689c80e08806b0154b5
Eddie Lee <devleehy@gmail.com>
2025-05-13 13:12:57 +0900

    refactor(User): User <-> Entity 연관관계 수정

--- src/main/java/com/gujo/uminity/user/entity/User.java
+++ src/main/java/com/gujo/uminity/user/entity/User.java
@@ -6,9 +6,7 @@ import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.JoinTable;
-import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import java.util.ArrayList;
 import java.util.List;
@@ -43,12 +41,7 @@ public class User {
     @Column(nullable = false, unique = true)
     private String phone;
 
-    @ManyToMany(fetch = FetchType.EAGER)
-    @JoinTable(
-            name = "users_roles",
-            joinColumns = @JoinColumn(name = "user_user_id"),
-            inverseJoinColumns = @JoinColumn(name = "roles_role_id")
-    )
+    @OneToMany(fetch = FetchType.EAGER)
     @ToString.Exclude
     private List<Role> roles = new ArrayList<>();
 

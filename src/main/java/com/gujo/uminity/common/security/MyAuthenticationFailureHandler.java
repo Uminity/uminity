69ba61317198323754000a66d1e7478fc5d06fec
Eddie Lee <devleehy@gmail.com>
2025-05-13 13:08:28 +0900

    feat: MyAuthenticationFailureHandler 구현
    - 인증에 실패할 경우 인증 정보에 있는 정보를 기반으로 ErrorResponse를 생성 후 response에 담아줌

---
+++ src/main/java/com/gujo/uminity/common/security/MyAuthenticationFailureHandler.java
@@ -0,0 +1,36 @@
+package com.gujo.uminity.common.security;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.gujo.uminity.common.ErrorResponse;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class MyAuthenticationFailureHandler implements AuthenticationFailureHandler {
+
+    private final ObjectMapper objectMapper;
+
+    @Override
+    public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
+
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        response.setContentType("application/json;charset=UTF-8");
+
+        ErrorResponse errorResponse = ErrorResponse.builder()
+                .status(HttpStatus.UNAUTHORIZED.value())
+                .code("AUTHENTICATION_FAILED")
+                .message("로그인에 실패했습니다.")
+                .detail(exception.getMessage())
+                .build();
+
+        objectMapper.writeValue(response.getWriter(), errorResponse);
+    }
+}

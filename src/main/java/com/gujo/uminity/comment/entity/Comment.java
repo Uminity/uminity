c78d97c2e9a6d190f2dc84e6fc18cc4a60e3f288
Iamcalmdown <ehsk2315@kyungsung.ac.kr>
2025-05-13 16:21:15 +0900

    feat: add JPA comment entity

--- src/main/java/com/gujo/uminity/comment/entity/Comment.java
+++ src/main/java/com/gujo/uminity/comment/entity/Comment.java
@@ -1,9 +1,16 @@
 package com.gujo.uminity.comment.entity;
 
-import java.util.UUID;
-import lombok.*;
+import com.gujo.uminity.post.entity.Post;
+import com.gujo.uminity.user.entity.User;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "comments")
@@ -12,16 +19,27 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 @AllArgsConstructor
 public class Comment {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "comment_id")
     private Long commentId;
 
-    @Column(name = "user_id", nullable = false)
-    private UUID userId;
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "post_id", nullable = false)
+    private Post post;
 
-    @Column(name = "post_id", nullable = false)
-    private Long postId;
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "parent_id")
+    private Comment parent;
+    // parent 필드가 null이면 최상위 댓글이고 답글은 parent가 가리키는 댓글의 children 리스트에 추가한다.
+
+    @OneToMany(mappedBy = "parent", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Comment> children = new ArrayList<>();
 
     @Column(columnDefinition = "TEXT")
     private String content;
@@ -29,3 +47,4 @@ public class Comment {
     @Column(name = "created_at")
     private LocalDateTime createdAt;
 }
+

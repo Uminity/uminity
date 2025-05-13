efb27765e565832640194c41a445bfff84c6640a
Iamcalmdown <ehsk2315@kyungsung.ac.kr>
2025-05-14 00:44:47 +0900

    feat: add CommentService 인터페이스 정의

--- src/main/java/com/gujo/uminity/comment/service/CommentService.java
+++ src/main/java/com/gujo/uminity/comment/service/CommentService.java
@@ -1,7 +1,23 @@
 package com.gujo.uminity.comment.service;
 
+import com.gujo.uminity.comment.dto.request.CommentCreateRequest;
+import com.gujo.uminity.comment.dto.request.CommentListRequest;
+import com.gujo.uminity.comment.dto.request.CommentUpdateRequest;
+import com.gujo.uminity.comment.dto.response.CommentResponseDto;
 import com.gujo.uminity.common.PageResponse;
 
 public interface CommentService {
-    PageResponse<Comm>
+    PageResponse<CommentResponseDto> listComments(Long postId, CommentListRequest req);
+    // 댓글 목록 조회
+
+    CommentResponseDto createComment(CommentCreateRequest req, String userId);
+
+    CommentResponseDto updateComment(Long commentId, CommentUpdateRequest req, String userId);
+
+    void deleteComment(Long commentId, String userId);
+
 }
+
+/*
+userId 인증 사용자 정보를 서비스 계층에서 받는다.
+ */

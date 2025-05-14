ebd3dcf0b5bb62107365d00c72a92e2db38b8811
Eddie Lee <devleehy@gmail.com>
2025-05-14 14:35:19 +0900

    feat: 마이페이지 유저 상세 정보 수정 컨트롤러 구현

--- src/main/java/com/gujo/uminity/mypage/controller/MyPageController.java
+++ src/main/java/com/gujo/uminity/mypage/controller/MyPageController.java
@@ -1,11 +1,16 @@
 package com.gujo.uminity.mypage.controller;
 
 import com.gujo.uminity.mypage.dto.MyPageResponseDto;
+import com.gujo.uminity.mypage.dto.UpdateUserInfoRequestDto;
 import com.gujo.uminity.mypage.service.MyPageService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -18,5 +23,11 @@ public class MyPageController {
     @GetMapping
     public ResponseEntity<MyPageResponseDto> getMyPageInfo() {
         return ResponseEntity.ok(myPageService.getMyPageInfo());
+    }
+
+    @PatchMapping
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void updateMyInfo(@RequestBody UpdateUserInfoRequestDto updateUserInfoRequestDto) {
+        myPageService.updateUserInfo(updateUserInfoRequestDto);
     }
 }

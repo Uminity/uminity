53fc0f5149a102c0337f1e1b147640878d8d1833
Iamcalmdown <ehsk2315@kyungsung.ac.kr>
2025-05-14 13:40:48 +0900

    feat: HTTP 요청 본문에 사용자 ID를 담지 않고, 시큐리티 컨텍스트로부터 꺼내오도록 해야 위·변조를 방지할 수 있다!!!!!!

--- src/main/java/com/gujo/uminity/post/controller/PostController.java
+++ src/main/java/com/gujo/uminity/post/controller/PostController.java
@@ -1,6 +1,7 @@
 package com.gujo.uminity.post.controller;
 
 import com.gujo.uminity.common.PageResponse;
+import com.gujo.uminity.common.security.MyUserDetails;
 import com.gujo.uminity.post.dto.request.PostCreateRequest;
 import com.gujo.uminity.post.dto.request.PostListRequest;
 import com.gujo.uminity.post.dto.request.PostUpdateRequest;
@@ -9,6 +10,7 @@ import com.gujo.uminity.post.service.PostService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
@@ -42,12 +44,19 @@ public class PostController {
     // 3. 게시글 생성
     @PostMapping
     public ResponseEntity<PostResponseDto> createPost(
-            @Valid @RequestBody PostCreateRequest req) {
-        PostResponseDto created = postService.createPost(req);
+            @Valid @RequestBody PostCreateRequest req,
+            @AuthenticationPrincipal MyUserDetails principal) {
+
+        String userId = principal.getUserId();
+        PostResponseDto created = postService.createPost(req, userId);
+//        PostResponseDto created = postService.createPost(req);
         return ResponseEntity
                 .created(URI.create("/api/posts/" + created.getPostId()))
                 .body(created);
     }
+    /*
+    HTTP 요청 본문에 사용자 ID를 담지 않고, 시큐리티 컨텍스트로부터 꺼내오도록 해야 위·변조를 방지할 수 있다!!!!!!!!!!!!
+     */
 
     // 4. 게시그 ㄹ수정
     @PutMapping("/{postId}")

3d4c331843b06f44657dd89867fa469741cb1e1e
Iamcalmdown <ehsk2315@kyungsung.ac.kr>
2025-05-14 09:30:27 +0900

    fix: @Builder.Default 사용 OneToMany 초기화

--- src/main/java/com/gujo/uminity/comment/entity/Comment.java
+++ src/main/java/com/gujo/uminity/comment/entity/Comment.java
@@ -37,6 +37,7 @@ public class Comment {
     @JoinColumn(name = "parent_id")
     private Comment parent;
 
+    @Builder.Default
     @OneToMany(mappedBy = "parent", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Comment> children = new ArrayList<>();
 

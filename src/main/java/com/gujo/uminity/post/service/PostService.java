a007dad227e4c636c1ec5596c9f40ab6e835e627
Iamcalmdown <ehsk2315@kyungsung.ac.kr>
2025-05-15 02:05:13 +0900

    feat: add incrementViewCount logic, cookie 검증

--- src/main/java/com/gujo/uminity/post/service/PostService.java
+++ src/main/java/com/gujo/uminity/post/service/PostService.java
@@ -5,6 +5,8 @@ import com.gujo.uminity.post.dto.request.PostCreateRequest;
 import com.gujo.uminity.post.dto.request.PostListRequest;
 import com.gujo.uminity.post.dto.request.PostUpdateRequest;
 import com.gujo.uminity.post.dto.response.PostResponseDto;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 public interface PostService {
     PageResponse<PostResponseDto> listPosts(PostListRequest req);
@@ -16,12 +18,17 @@ public interface PostService {
     PostResponseDto updatePost(Long postId, PostUpdateRequest request, String userId);
 
     void deletePost(Long postId, String userId);
+
+    /*
+    DIP 의존해서 컨트롤러가 구현체가 아니라 역할에만 의존하도록 캐싱 처리
+
+    인증된 사용자만 자신의 글을 생성 수정 삭제할 수 있게 하려면
+    서비스 계층에서 userId를 받아와야한다.
+    */
+
+    void incrementViewCount(Long postId, HttpServletRequest request, HttpServletResponse response);
+    /* 조회수는 누가봤는지 검증안해도되고 postId만 검증
+    stateless 한 쿠키로 중복 카운팅 방지 - 세션 방식과 쿠키 방식중
+     */
 }
 
-/*
- DIP 의존해서 컨트롤러가 구현체가 아니라 역할에만 의존하도록 캐싱 처리
-
-
- 인증된 사용자만 자신의 글을 생성 수정 삭제할 수 있게 하려면
- 서비스 계층에서 userId를 받아와야한다.
- */

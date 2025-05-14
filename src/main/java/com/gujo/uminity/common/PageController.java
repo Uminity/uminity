217eec4721426923ee8d21f9a9cb3b89b5e4ecea
Jisan <kjs25481@gmail.com>
2025-05-14 09:43:33 +0900

    postDetail 및 index 보완

--- src/main/java/com/gujo/uminity/common/PageController.java
+++ src/main/java/com/gujo/uminity/common/PageController.java
@@ -2,6 +2,8 @@ package com.gujo.uminity.common;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class PageController {
@@ -20,4 +22,11 @@ public class PageController {
     public String register() {
         return "register.html";
     }
+    
+    @GetMapping("/postDetail/{postId}")
+    public String postDetail(@PathVariable("postId") String postId) {
+    	System.out.println(postId);
+        return "/postDetail.html";
+    }
+    
 }

6c28c0d3d5cc377625832d51ce1ce4de781875d9
Iamcalmdown <ehsk2315@kyungsung.ac.kr>
2025-05-15 17:36:32 +0900

    feat: add LikerService 사용자 이름반환 페이지네이션

--- src/main/java/com/gujo/uminity/like/service/LikeService.java
+++ src/main/java/com/gujo/uminity/like/service/LikeService.java
@@ -1,10 +1,14 @@
 package com.gujo.uminity.like.service;
 
 import com.gujo.uminity.like.dto.response.ToggleLikeResponse;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 public interface LikeService {
 
     ToggleLikeResponse toggleLike(String userId, Long postId);
+
+    Page<String> getLikerNamesByPostId(Long postId, Pageable pageable);
 }
 
 // 좋아요 결과 응답 DOT 반환

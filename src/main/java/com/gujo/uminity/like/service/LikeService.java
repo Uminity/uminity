61ed5b051da5e318783cf7d5fba85ef9c2441dc0
Iamcalmdown <ehsk2315@kyungsung.ac.kr>
2025-05-16 00:07:22 +0900

    feat: add Pagination PageResponse 반환<이름>, <응답DTO>

--- src/main/java/com/gujo/uminity/like/service/LikeService.java
+++ src/main/java/com/gujo/uminity/like/service/LikeService.java
@@ -1,15 +1,20 @@
 package com.gujo.uminity.like.service;
 
+import com.gujo.uminity.common.web.PageResponse;
 import com.gujo.uminity.like.dto.response.ToggleLikeResponse;
-import org.springframework.data.domain.Page;
+import com.gujo.uminity.post.dto.response.PostResponseDto;
 import org.springframework.data.domain.Pageable;
 
 public interface LikeService {
 
     ToggleLikeResponse toggleLike(String userId, Long postId);
 
-    Page<String> getLikerNamesByPostId(Long postId, Pageable pageable);
+    PageResponse<String> getLikerNamesByPostId(Long postId, Pageable pageable);
+
+    PageResponse<PostResponseDto> getMyLikedPosts(String userId, Pageable pageable);
 }
 
 // 좋아요 결과 응답 DOT 반환
+// 게시글에 좋아요 누른 사용자 이름 목록 조회
+// 사용자(내)가 좋아요한 게시글 목록 조회
 // DIP 준수

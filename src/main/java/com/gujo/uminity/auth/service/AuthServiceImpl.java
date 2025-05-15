8a268ea66f818f25954eb4b8e37dc5d29f7b28c9
Eddie Lee <devleehy@gmail.com>
2025-05-16 01:48:52 +0900

    feat: 예외 로직 추가
    - 이미 사용 중인 휴대폰 번호 예외 추가

--- src/main/java/com/gujo/uminity/auth/service/AuthServiceImpl.java
+++ src/main/java/com/gujo/uminity/auth/service/AuthServiceImpl.java
@@ -9,6 +9,7 @@ import com.gujo.uminity.user.repository.RoleRepository;
 import com.gujo.uminity.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -25,12 +26,12 @@ public class AuthServiceImpl implements AuthService {
 
     @Override
     public RegisterResponseDto register(RegisterRequestDto registerRequestDto) {
-
         if (userRepository.existsByEmail(registerRequestDto.getEmail())) {
             throw new IllegalArgumentException("이미 사용 중인 이메일입니다.");
         }
 
         User user = new User();
+
         Role findRole = roleRepository.findRoleByRoleName(USER);
 
         user.setName(registerRequestDto.getName());
@@ -38,10 +39,13 @@ public class AuthServiceImpl implements AuthService {
         user.setPassword(passwordEncoder.encode(registerRequestDto.getPassword()));
         user.setPhone(registerRequestDto.getPhone());
         user.getRoles().add(findRole);
-        userRepository.save(user);
 
-        return RegisterResponseDto.builder()
-                .result(ResultStatus.SUCCESS)
-                .build();
+        try {
+            userRepository.save(user);
+        } catch (DataIntegrityViolationException e) {
+            throw new IllegalArgumentException("이미 사용 중인 휴대폰 번호 입니다.");
+        }
+
+        return new RegisterResponseDto(ResultStatus.SUCCESS);
     }
 }

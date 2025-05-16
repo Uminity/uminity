389ac740afa6a9d80d9780e0e729309bd9aa6647
Eddie Lee <devleehy@gmail.com>
2025-05-16 22:58:00 +0900

    feat: 기능 추가

--- src/main/resources/static/assets/js/util.js
+++ src/main/resources/static/assets/js/util.js
@@ -175,7 +175,7 @@ function makePaginationHTML(listRowCount, pageLinkCount, currentPageIndex, total
     }
 
     let next;
-    if (endPageIndex > pageCount) {
+    if (endPageIndex > pageLinkCount) {
         endPageIndex = pageCount
         next = false;
     } else {
